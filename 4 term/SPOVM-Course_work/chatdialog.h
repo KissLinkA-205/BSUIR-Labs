068f802b627e92f5b0b86c9334d4fabe01b6bf97
Анжелика Деркач <71188712+KissLinkA-205@users.noreply.github.com>
2021-05-10 00:21:13 +0300

    Add files via upload

---
+++ 4 term/SPOVM-Course_work/chatdialog.h
@@ -0,0 +1,30 @@
+#ifndef CHATDIALOG_H
+#define CHATDIALOG_H
+
+#include "ui_chatdialog.h"
+#include "client.h"
+
+class ChatDialog : public QDialog, private Ui::ChatDialog
+{
+    Q_OBJECT
+
+public:
+    ChatDialog(QWidget *parent = nullptr);
+
+public slots:
+    void appendMessage(const QString &from, const QString &message);
+
+private slots:
+    void returnPressed();
+    void newParticipant(const QString &nick);
+    void participantLeft(const QString &nick);
+    void showInformation();
+    void on_connectButton_clicked();
+
+private:
+    Client client;
+    QString myNickName;
+    QTextTableFormat tableFormat;
+};
+
+#endif
