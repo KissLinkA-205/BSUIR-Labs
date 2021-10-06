6229a25d69061c4136a882a00b6ce2b5a5338a67
Анжелика Деркач <71188712+KissLinkA-205@users.noreply.github.com>
2021-10-06 13:12:17 +0300

    Added the third lab by IiPU

---
+++ 5 term/IiPU/Lab_3/libraries.h
@@ -0,0 +1,10 @@
+#pragma comment(lib, "PowrProf.lib")
+#pragma comment (lib, "setupapi.lib")
+#include <Windows.h>
+#include <iostream>
+#include <powrprof.h>
+#include <thread>   
+#include "conio.h"
+#include <Poclass.h>
+#include <Setupapi.h>
+#include <devguid.h>
