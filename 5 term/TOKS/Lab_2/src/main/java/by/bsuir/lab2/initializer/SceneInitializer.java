b9747d4a2141f3f854d27b6317d653b13d73a167
Анжелика Деркач <71188712+KissLinkA-205@users.noreply.github.com>
2021-10-06 13:17:04 +0300

    Added the second lab by TOKS

---
+++ 5 term/TOKS/Lab_2/src/main/java/by/bsuir/lab2/initializer/SceneInitializer.java
@@ -0,0 +1,23 @@
+package by.bsuir.lab2.initializer;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class SceneInitializer {
+    private static final String RESOURCE = "/main.fxml";
+    private static final String TITLE = "COM-port transmitter";
+
+    public void initScene(Stage stage) throws IOException {
+        URL url = getClass().getResource(RESOURCE);
+        Parent root = FXMLLoader.load(url);
+        stage.setScene(new Scene(root));
+        stage.setResizable(false);
+        stage.setTitle(TITLE);
+        stage.show();
+    }
+}
